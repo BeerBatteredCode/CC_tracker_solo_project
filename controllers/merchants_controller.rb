ab5e021c1bdf5edc3b12d1b0fe3b0e8edc2b1d53
Grant Rutherford <contact.grantr@gmail.com>
2019-03-23 16:28:28 +0000

    Starting merchant erb files

--- controllers/merchants_controller.rb
+++ controllers/merchants_controller.rb
@@ -2,3 +2,13 @@ require('sinatra')
 require('sinatra/contrib/all')
 require_relative('../models/merchant.rb')
 also_reload('../models/*')
+
+get '/merchant' do
+  @victims = Victim.all()
+  erb ( :"victims/index" )
+end
+
+get '/victims/:id' do
+  @victim = Victim.find(params['id'].to_i)
+  erb( :"victims/show" )
+end
