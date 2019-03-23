c44133dcc3118790dd20bc40f7ade6723ea10873
Grant Rutherford <contact.grantr@gmail.com>
2019-03-23 17:38:35 +0000

    global index complete

--- controllers/merchants_controller.rb
+++ controllers/merchants_controller.rb
@@ -3,12 +3,12 @@ require('sinatra/contrib/all')
 require_relative('../models/merchant.rb')
 also_reload('../models/*')
 
-get '/merchant' do
-  @merchant_list = Victim.all()
-  erb ( :"merchant/index" )
+get '/merchants' do
+  @merchant_list = Merchant.all()
+  erb ( :"merchants/index" )
 end
 
-get '/merchant/:id' do
+get '/merchants/:id' do
   @merchant = Merchant.find(params['id'].to_i)
   erb( :"merchants/show" )
 end
