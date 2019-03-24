4cd61f005ef08d4b717b9cd3f009fb876bb494d5
Grant Rutherford <contact.grantr@gmail.com>
2019-03-24 23:40:38 +0000

    delete function working on individual tags/merchants

--- controllers/stamps_controller.rb
+++ controllers/stamps_controller.rb
@@ -3,12 +3,27 @@ require('sinatra/contrib/all') if development?
 require_relative('../models/stamp.rb')
 also_reload('../models/*')
 
+#INDEX
 get '/stamps' do
   @stamp_list = Stamp.all()
   erb (:"stamps/index")
 end
 
+#NEW
 get '/stamps/:id' do
   @stamp = Stamp.find(params['id'].to_i)
   erb(:"stamps/show")
+end
+
+#SAVE
+post '/stamps' do
+  stamp = Stamp.new(params)
+  stamp.save
+  redirect to("/stamps")
+end
+
+#DELETE
+post '/stamps/:id/delete' do
+  Stamp.destroy(params[:id])
+  redirect to("/stamps")
 end
