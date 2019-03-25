5f4b23dcfba69a4e450e66a0c2da5f7f9ec5794d
Grant Rutherford <contact.grantr@gmail.com>
2019-03-25 12:02:08 +0000

    Changes made to database name

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,7 +4,7 @@ class SqlRunner
 
   def self.run( sql, values = [] )
     begin
-      db = PG.connect({ dbname: 'guard_dog', host: 'localhost' })
+      db = PG.connect({ dbname: 'pawpal', host: 'localhost' })
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
     ensure
